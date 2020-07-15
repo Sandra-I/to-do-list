5f38343bc57b6ded2ef7adb07d3aa2267e39aeb9
S-I <sandra.ibrahim@hotmail.fr>
2020-07-15 22:55:27 +0200

    Méthodes authentification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,44 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  // Méthode pour la création d'un nouvel utilisateur
+  // Utilisation de Promise car c'est une méthode asynchrone
+  //  .auth permet d'avoir accès aux méthodes d'authentification
+  createNewUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Méthode pour la connexion d'un utlisateur
+  signInUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Méthode pour se déconnecter
+  signOutUser() {
+    firebase.auth().signOut();
+  }
+
 }
